fe82cf53571c9f9d7fe85fb0367ac27487619e9b
Mayank Negi <mayank.negi0@gmail.com>
2021-05-28 13:50:48 +0530

    Api configurated and boilercode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
 import './App.css'
 
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
 function App(){
+
+    const [coins,setCoins] = useState([])
+
+    useEffect(()=>{
+        axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+        .then(res=>{
+            setCoins(res.data)
+        }).catch(error =>console.log("error"))
+    })
+
     return(
-        <div className="App">
-            <h1>APIIII</h1>
+        <div className="coin-app">
+            <div className="coin-search">
+                <h1 className="coin-text">Search a currency</h1>
+                <form>
+                <input type="text" placeholder="Search" className="coin-input"/>
+                </form>
+            </div>
         </div>
     )
 }
